6e5bed258820e680baaa2597531a305b39687232
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2022-03-07 16:39:28 +0530

    Added Stacks

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -2,4 +2,5 @@
 
 # Script to build Github Pages
 python3 -m nbconvert --to html --template lab --output-dir "docs" "Python - MySQL Connectivity.ipynb"
+python3 -m nbconvert --to html --template lab --output-dir "docs" "Python - Stacks and Queues.ipynb"
 
